a8b1b8b3feb8196a72a39253deb41b040bdfe3cd
revking <pankajrox.yadav@gmail.com>
2020-12-09 13:47:38 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { HighchartsChartComponent } from 'highcharts-angular';
-import {PiechartComponent} from 'src/app/piechart/piechart.component';
+import { HighchartsChartComponent } from "highcharts-angular";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { from } from 'rxjs';
+import { AppComponent } from "./app.component";
+import { from } from "rxjs";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HighchartsChartComponent,
-    PiechartComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HighchartsChartComponent,
     RouterModule.forRoot([
-      {path: 'piechart', component: PiechartComponent},
       //{path: 'barchart', component: HighchartsChartComponent}
-    ]),
-    AppRoutingModule,
-    
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
